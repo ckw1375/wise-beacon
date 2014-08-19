fa76422bdbb89bf2ca12cf3f210c1471e1148d6e
Mingook <migenii@gmail.com>
2014-08-19 17:16:11 +0900

    delete

--- EstimoteSample/src/com/estimote/sdk/connection/BluetoothService.java
+++
@@ -1,9 +0,0 @@
-package com.estimote.sdk.connection;
-
-import android.bluetooth.BluetoothGattCharacteristic;
-
-public abstract interface BluetoothService
-{
-  public abstract void update(BluetoothGattCharacteristic paramBluetoothGattCharacteristic);
-}
-
